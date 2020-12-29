db29c950684a5d6636c2dc043eabefd706ec6989
Joseph Buell <jlrbuellv@gmail.com>
2020-12-29 13:45:03 -0500

    All days complete

--- day23/main.py
+++ day23/main.py
@@ -27,11 +27,61 @@ def part1(data, iterations=100):
 
     one = data.index(1)
     return ''.join([str(x) for x in data[one:] + data[:one]])[1:]
-def part2(data):
-    data = data[:]
-    while len(data) < 1000000:
-        data.append(len(data) + 1)
-    result = part1(data, iterations=10000)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.successor = None
+        self.destination = None
+
+def print_thing(node):
+    x = node.successor
+    s = str(node.value)
+    while x != node:
+        s += str(x.value)
+        x = x.successor
+    print(s)
+
+def part2(data, iterations = 10000000):
+    nodes = {}
+    prev = None
+    for v in data:
+        current = Node(v)
+        nodes[v] = current
+        if prev:
+            prev.successor = current
+        prev = current
+    for node in nodes.values():
+        node.destination = nodes.get(node.value - 1)
+    
+    prev.successor = Node(10)
+    prev = prev.successor
+    prev.destination = nodes[9]
+    for i in range(11, 1000000 + 1):
+        current = Node(i)
+        prev.successor = current
+        current.destination = prev
+        prev = current
+    prev.successor = list(nodes.values())[0]
+    nodes[1].destination = prev
+
+    current = prev.successor
+    for iteration in range(iterations):
+        destination = current.destination
+        moving = []
+        moved = current
+        for i in range(3):
+            moving.append(moved.successor)
+            moved = moved.successor
+        while destination in moving:
+            destination = destination.destination
+
+        current.successor = moving[-1].successor
+        destination.successor, moving[-1].successor = moving[0], destination.successor
+        current = current.successor
+    n = nodes[1].successor
+    return n.value * n.successor.value
+
 
 print(part1(data))
 print(part2(data))
